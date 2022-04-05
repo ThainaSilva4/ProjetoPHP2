b38c30679ce64af7b4ce30b9763909357e28f872
Thainá Ferreira <thaina.silva254@etec.sp.gov.br>
2022-04-05 13:24:33 -0300

    segunda versão

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
 require_once("pessoa.class.php");
+require_once("empresa.class.php");
 
 $pessoa = new Pessoa();
 $pessoa->setNome("Thainá Ferreira da Silva");
+$pessoa->setEndereco("<br> Rua Desconhecida.");
 echo $pessoa->getNome();
+echo $pessoa->getEndereco();
 
+$empresa = new Empresa();
+$empresa->setRazaoSocial("<br> Escola XPTO LTDA");
+echo $empresa->getRazaoSocial();
 ?>
